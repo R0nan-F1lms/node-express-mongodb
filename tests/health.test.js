a73bd0db7f76947688c6a817dfee2d28e1a5a5f7
R0nan-F1lms <ronan.winfield@gmail.com>
2025-09-22 13:48:16 +1000

    added in eslintric for code quality scanning

--- tests/health.test.js
+++ tests/health.test.js
@@ -6,7 +6,7 @@ describe('API Health Check', () => {
   it('should return 200 on root endpoint', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('{\"message\":\"Welcome to bezkoder application.\"}');
+    expect(res.body).toEqual({ message: 'Welcome to bezkoder application.' });
   });
 });
 afterAll(async () => {
